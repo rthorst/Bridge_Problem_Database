c83c18526c871c7bd81830e970b7ca372c3463d4
Robert Thorstad <thorstadrs@gmail.com>
2020-05-06 13:50:26 -0400

    add some code to show hand in markdown

---
+++ frontend.py
@@ -0,0 +1,9 @@
+import streamlit
+
+CLUB = "&#9827;"
+DIAMOND = "&#9826;"
+HEART = "&#9825;"
+SPADE = "&#9824;"
+
+NORTH_MARKDOWN = "{} A9743\n{} K8763\n{} A6\n{}7".format(
+        SPADE, HEART, DIAMOND, CLUB)
